ccdb1aa41470d2cf87c26c3e304a304fa9a52b3e
Meghna2508 <meghnanagaraj25@gmail.com>
2026-05-21 04:04:39 -0800

    fix: hardcode render url in api

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://smart-leads-dashboard-1-kh9n.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 15000,
 });
